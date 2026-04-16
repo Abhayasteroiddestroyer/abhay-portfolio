88c5cd2bb3c98781d3a905d55a0d7ff849a610ea
Abhayasteroiddestroyer <abhaychauhandav37@gmail.com>
2026-04-16 18:14:01 +0530

    Add files via upload
    Second commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Initialize Scroll Animations
+AOS.init({
+    duration: 1000,
+    offset: 100,
+});
+
+// Typing Effect
+var typed = new Typed('#typing', {
+    strings: ['B.Tech Student', 'Web Developer', 'Tech Enthusiast'],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true
+});
